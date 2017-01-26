a6689d571e05f24b88c682cdfd6c8d5336592565
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-01-26 18:06:52 +0100

    ajout api météo

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 (function(){
-	console.log('Start');
-
-
-	$('#sendAjaxBtn').click(function(){
-		console.log('click btn');
-
-		var url = "http://localhost:3000/places";
-		$.ajax(url).done(ajaxDone).fail(ajaxFailed);
+	// console.log('Start');
+	// console.log('click btn');
+	// var url = "http://localhost:3000/places";
+	// $.ajax(url).done(ajaxDone).fail(ajaxFailed);
+	$.ajax({
+		url: "http://api.openweathermap.org/data/2.5/weather?q=Toulouse,France&appid=48b208710333f43248240ee7fbd57077&lang=fr&units=metric",
+		success: function(data){
+			// var meteo = JSON.parse(body);
+			var temp = data.main.temp;
+			var humidity = data.main.humidity;
+			$('#receiver').append('a Toulouse il fait ' + temp + '&#176;C et il y a ' + humidity + "% d'humidit&#233e");
+		}
 	});
 
-	function ajaxDone(data, status){
-		console.log('done', data);
-		var places = data.places;
-		console.log(places);
-
-		for(var i=0; i<places.length; i++){
-			var place = places[i];
-			var newElement = "<li>" + place.nom +"</li>";
-			
-			if(place.nom === "IoT Valley"){
-				var motDePasse = place.password;
-				$('body').append('Mot de passe: ' + motDePasse);
-			}
-
-			$('#list').append(places);
-		}
-	};
-
-	function ajaxFailed(){
-		console.log('failed');
-
-	};
 })();
+
+// function ajaxDone(data, status){
+// 	console.log('done', data);
+// 	var places = data.places;
+// 	console.log(places);
+
+// 	for(var i=0; i<places.length; i++){
+// 		var place = places[i];
+// 		var newElement = "<li>" + place.nom +"</li>";
+
+// 		if(place.nom === "IoT Valley"){
+// 			var motDePasse = place.password;
+// 			$('body').append('Mot de passe: ' + motDePasse);
+// 		}
+
+// 		$('#list').append(places);
+// 	}
+// };
+
+// function ajaxFailed(){
+// 	console.log('failed');
+
+// };
--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function (req, res) {
-	var meteoUrl = 	"http://api.openweathermap.org/data/2.5/weather?q=Toulouse,France&appid=48b208710333f43248240ee7fbd57077&lang=fr&units=metric"
-	require('request').get(meteoUrl, function(err, response, body){
-		var meteo = JSON.parse(body);
-		res.send(meteo)
-	})
+	// var meteoUrl = 	"http://api.openweathermap.org/data/2.5/weather?q=Toulouse,France&appid=48b208710333f43248240ee7fbd57077&lang=fr&units=metric"
+	// require('request').get(meteoUrl, function(err, response, body){
+	// 	var meteo = JSON.parse(body);
+		res.send('GG')
+	// })
 
 });
 
